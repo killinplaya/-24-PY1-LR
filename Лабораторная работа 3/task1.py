fd493fa82358933fcf81e3de747cd4b25019041f
astra <sadboyyy3332@gmail.com>
2024-11-03 16:02:47 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,17 @@
+def find_first_index(items, target):
+    #Находит индекс первого вхождения товара в списке.
+    if target in items:
+        return items.index(target)
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_first_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
+
+
