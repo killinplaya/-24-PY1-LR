7f2bc9f8affbaa869031b363fbf20f759c1eedb3
astra <sadboyyy3332@gmail.com>
2024-11-05 11:07:56 +0300

    Add files via upload

---
+++ Лабораторная работа 4/task1.py
@@ -0,0 +1,22 @@
+import json
+
+def task() -> float:
+
+    # Открываем JSON файл и загружаем данные
+    with open('input.json', 'r') as file:
+        data = json.load(file)  # data будет списком словарей
+
+    # Переменная для накопления суммы произведений
+    total_sum = 0.0
+
+    # Проходим по каждому словарю в списке
+    for item in data:
+        # Извлекаем значения "score" и "weight" и вычисляем произведение
+        product = item["score"] * item["weight"]
+        # Добавляем произведение к общей сумме
+        total_sum += product
+
+    # Возвращаем сумму произведений, округленную до 3 знаков
+    return round(total_sum, 3)
+
+print(task())
