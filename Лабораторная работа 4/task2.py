13ffd8ce4eff9529411cb03eb09e1360274285c9
astra <sadboyyy3332@gmail.com>
2024-11-05 11:05:25 +0300

    Add files via upload

---
+++ Лабораторная работа 4/task2.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+
+    # Читаем данные из CSV-файла
+    with open(INPUT_FILENAME, mode='r', newline='') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        data = [row for row in reader]  # Список словарей для каждой строки CSV
+
+    # Записываем данные в JSON-файл
+    with open(OUTPUT_FILENAME, mode='w') as json_file:
+        json.dump(data, json_file, indent=4)  # Запись с отступами для форматирования
+
+
+if __name__ == '__main__':
+    # Вызов функции для выполнения задания
+    task()
+
+    # Чтение и вывод содержимого выходного JSON-файла
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
